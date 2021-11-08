60076469801d2c466acca945106b1c1a24967bbd
santiago-jv <santiagoolayojv@gmail.com>
2021-11-07 19:06:25 -0500

    more changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 
 import './App.css';
 import { useState } from "react"
-const  words = {
-	spanish:{
-		title:"Automata",
-		textButton:"español"
+import words from "./words.json" 
 
-	},
-
-	english:{
-		title:"Automaton",
-		textButton:"Inglés"
-	}
-}
 function App() {
 	let count = 0
 	const [language, setLanguage] = useState("spanish")
+	let aux = []
 	let isAccept = false
-	const [string, setString] = useState(null)
+	const [string, setString] = useState("")
+	const [result, setResult] = useState(null)
+	
 
 	const start = () => {
 		count = 0
 		isAccept = false
+		setResult("xd")
+		aux = []
 		state_0()
 	}
 
 	const state_0 = () => {
+		
 		console.log("state_0")
-		if (string.length > count) {
+		if (isNotOutRange()) {
 			if (string[count] === 'a') {
 				count++
 				state_1()
@@ -39,14 +35,25 @@ function App() {
 		}else{
 			if (string.length === 0) {
 				isAccept = true
+				setResult("Es aceptado")
 			}
 		}
 		
 	}
 
+	function isNotOutRange() {
+		const isValid = string.length > count
+		console.log("IsAccept:",isAccept);
+		if(!isValid) {
+			setResult(isAccept? "Es aceptado": "Rechazado")
+			
+		}
+		return isValid
+
+	}
 	const state_1 = () => {
 		console.log("state_1")
-		if (string.length > count) {
+		if (isNotOutRange()) {
 			if (string[count] === 'b') {
 				count++
 				state_2()
@@ -59,17 +66,19 @@ function App() {
 
 	const state_2 = () => {
 		console.log("state_2")
-		if (string.length > count) {
+		if (isNotOutRange()) {
 			if (string[count] === 'c') {
 				count++
 				state_3()
 				
-			}else{
-				if (string[count] === 'd') {
+			}
+			else if (string[count] === 'd') {
 					count++
 					state_4()
-					
-				}
+				
+			}
+			else {
+				error()
 			}
 		}
 	}
@@ -77,30 +86,39 @@ function App() {
 	const state_3 = () => {
 		
 		console.log("state_3")
-		if (string.length > count) {
+		if (isNotOutRange()) {
+	
+			if (string[count] === 'c') {
+				count++
+				state_3()
+				
+			} else if (string[count] === 'd') {
+				count++
+				state_4()
 			
-			
-				if (string[count] === 'c') {
-					count++
-					state_3()
-					
-				} else {
-					if (string[count] === 'd') {
-						count++
-						state_4()
-						
-					}
-				}
-		
-			
-
+			}	
+			else{
+				error()
+			}
+	
 		}
 	}
 
-	const state_4 = () => {
+	/* const state_4 = () => {
 		console.log("state_4")
-		if (string.length > count) {
-			if (string[count] === 'a') {
+		isAccept = true
+		console.log("count",count);
+		if (isNotOutRange()) {
+			console.log("***** auxiliar:",aux,"********");
+			if((aux &&  aux !== string.slice(aux.length, count))) {
+				console.log("entra");
+				error()
+			}
+			
+			else if (string[count] === 'a') {
+			
+				aux = string.slice(aux?aux.length:0,count)
+				console.log("entra por aca xd", aux);
 				count++
 				state_1()
 				
@@ -110,22 +128,89 @@ function App() {
 			}
 			
 		}
-		isAccept = true
+		else {
+			console.log("auxiliar:", aux);
+			if(aux) console.log(string.slice(aux.length, count))
+			if((aux &&  aux !== string.slice(aux.length, count)  )) {
+				console.log("entra");
+				error()
+			}
+		}
+	
 	}
-
+ */
+	const state_4 = () => {
+		console.log("state_4")
+		isAccept = true
+		aux.push(string.slice(aux?aux.length:0,count))
+		console.log("count",count);
+		if (isNotOutRange()) {
+			console.log("***** auxiliar:",aux,"********");
+			for (let index = 0; index < aux.length; index++) {
+				const element = aux[index];
+				const nextElement = aux[index+1]
+				if(element && nextElement  ){
+					if(element !== nextElement){
+						error()
+					}
+				}
+		
+				
+			}
+		
+			if (string[count] === 'a') {
+			
+				aux.push( string.slice(aux?aux.length:0,count))
+				console.log("entra por aca xd", aux);
+				count++
+				state_1()
+				
+			}else{
+				error()
+				
+			}
+			
+		}
+		else {
+		/* 	console.log("auxiliar:", aux);
+			if(aux) console.log(string.slice(aux.length, count))
+			if((aux &&  aux !== string.slice(aux.length, count)  )) {
+				console.log("entra");
+				error()
+			} */
+		}
+	
+	}
 	const error = () => {
+		isAccept=false
+		setResult("Rechazado")
 		console.log("error")
 		
 	}
 	const handleInputChange = (event) => {
 		setString(event.target.value)
 	}
+	const verify = (event) => {
+		event.preventDefault()
+		start()
+	}
 	return (
 		<>	
-			<h1>{words[language].title}</h1>
-			<input onChange={handleInputChange} type="text"/>
-			<button onClick={start}>{words[language].textButton}</button>
-			<button onClick={()=>setLanguage("spanish"===language? "english":"spanish")}>change language</button>
+			<main>
+				<h1>{words[language].title}</h1>
+				<form onSubmit={verify}>
+				<div className="form-group">
+					<label htmlFor="text">{words[language].label}</label>
+					<input id="text" onChange={handleInputChange} type="text"/>
+				</div>
+				<button type="submit">{words[language].textButton}</button>
+				</form>
+				<button onClick={()=>setLanguage("spanish"===language? "english":"spanish")}>
+					{words[language].changeLanguage}
+				</button>
+
+				<h1>{result && result}</h1>
+			</main>
 		</>
 	);
 }
